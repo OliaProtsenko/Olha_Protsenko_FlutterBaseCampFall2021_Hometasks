b6a7b489437be3534bb70a80cd43b7814f616a98
Olia <iamola88093@gmail.com>
2021-11-06 15:07:29 +0200

    Implement the doSomeHeavyWork method in the ComputeHeavyTaskPerformer class using the compute function, and also  the same method in the SpawnedIsolateTaskPerformer class using spawned

--- lib/hash_calculator/spawned_isolate_task_performer.dart
+++ lib/hash_calculator/spawned_isolate_task_performer.dart
@@ -1,15 +1,49 @@
 import 'dart:async';
 import 'dart:isolate';
 
+
+import 'dart:isolate';
+import 'package:lecture_2_hometask_starter/constants.dart';
 import 'package:lecture_2_hometask_starter/hash_calculator/heavy_task_performer.dart';
+import 'package:lecture_2_hometask_starter/helpers/random_number_hash_calculator.dart';
 
 class SpawnedIsolateTaskPerformer implements HeavyTaskPerformer {
-
-  Isolate? _isolate;
-  Completer? _completer;
+  late Isolate _isolate;
+  late Completer<String> _completer;
 
   @override
-  Future<String> doSomeHeavyWork() {
+  Future<String> doSomeHeavyWork() async {
+    _completer = Completer<String>();
+    try {
+      final spawnerReceivePort = ReceivePort();
+      _isolate = await Isolate.spawn(
+        _establishCommunicationWithSpawner,
+        spawnerReceivePort.sendPort,
+      );
+      spawnerReceivePort.listen((message) {
+        if (message is SendPort) {
+          message.send(DefaultIterationsCount);
+        } else if (message is String) {
+          _completer.complete(message);
+        }
+      });
+    } catch (e) {
+      _completer.completeError(e);
+    }
+    return _completer.future;
     throw UnimplementedError();
+  }
+
+  static void _establishCommunicationWithSpawner(SendPort spawnerSendPort) {
+    final spawneeReceicePort = ReceivePort();
+    spawnerSendPort.send(spawneeReceicePort.sendPort);
+    spawneeReceicePort.listen((message) {
+      if (message is int) {
+        final hashCalculate = RandomNumberHashCalculator();
+        final result =
+        hashCalculate.calculateRandomNumberHash(iterationsCount: message);
+        spawnerSendPort.send(result);
+      }
+    });
   }
 }
