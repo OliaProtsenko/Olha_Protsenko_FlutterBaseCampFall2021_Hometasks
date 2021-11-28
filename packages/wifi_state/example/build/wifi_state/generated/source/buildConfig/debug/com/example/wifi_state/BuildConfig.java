f98ce6b5f9e4b72ccf5853b261f693bab9e1367c
Olia <iamola88093@gmail.com>
2021-11-28 12:18:10 +0200

    start

---
+++ packages/wifi_state/example/build/wifi_state/generated/source/buildConfig/debug/com/example/wifi_state/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.wifi_state;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.wifi_state";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ packages/wifi_state/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<wifi_state/WifiStatePlugin.h>)
+#import <wifi_state/WifiStatePlugin.h>
+#else
+@import wifi_state;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [WifiStatePlugin registerWithRegistrar:[registry registrarForPlugin:@"WifiStatePlugin"]];
+}
+
+@end
